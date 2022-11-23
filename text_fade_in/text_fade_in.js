d881a6b01607fac9bc4b972cf9c817258e08b05f
marshallprogramming <42378943+marshallprogramming@users.noreply.github.com>
2022-11-23 21:18:24 +0000

    Add Text Fade In functionality

---
+++ text_fade_in/text_fade_in.js
@@ -0,0 +1,42 @@
+var ce = "createElement",
+  qs = "querySelector";
+
+function buildThresholdList() {
+  let thresholds = [];
+  let numSteps = 20;
+
+  for (let i = 1.0; i <= numSteps; i++) {
+    let ratio = i / numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
+}
+
+var observer = new IntersectionObserver(
+  function intersectionCallback(entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        if (
+          entry.intersectionRatio > 0.3 &&
+          !elem.classList.contains("appear")
+        ) {
+          elem.classList.add("appear");
+        }
+      }
+    });
+  },
+  { root: null, rootMargin: "100px", threshold: buildThresholdList() }
+);
+
+(function (d) {
+  var textChunks = d[qs + "All"](
+    ".Theme-Layer-BodyText--inner p, .Theme-Layer-BodyText--inner h5, .Theme-Layer-BodyText--inner h4, .Theme-Layer-BodyText--inner h3, .Theme-Layer-BodyText--inner h2, .Theme-Layer-BodyText--inner h1"
+  );
+
+  textChunks.forEach(function (textChunk) {
+    observer.observe(textChunk);
+  });
+})(document);
