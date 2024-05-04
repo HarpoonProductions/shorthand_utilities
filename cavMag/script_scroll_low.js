ac5d20b59497f5dadf4476101d20f4cb870464ff
HarpoonProductions <110096521+HarpoonProductions@users.noreply.github.com>
2024-05-04 16:37:57 +0100

    Update script_scroll_low.js

--- cavMag/script_scroll_low.js
+++ cavMag/script_scroll_low.js
@@ -230,7 +230,8 @@
     navContainer.classList.add("nav_container");
 
     const prevUrl = currentIndex > 0 ? links[currentIndex - 1].href : null;
-    const prevText = currentIndex > 0 ? links[currentIndex - 1].label : null;
+    // const prevText = currentIndex > 0 ? links[currentIndex - 1].label : null;
+    const prevText = currentIndex > 0 ? "Previous" : null;
 
     const prevButton = createButtonWithImage(
       prevText,
@@ -268,9 +269,9 @@
 
     const nextUrl =
       currentIndex < links.length - 1 ? links[currentIndex + 1].href : null;
-    const nextText =
-      currentIndex < links.length - 1 ? links[currentIndex + 1].label : null;
-
+    // const nextText =
+    //   currentIndex < links.length - 1 ? links[currentIndex + 1].label : null;
+    const nextText = currentIndex < links.length - 1 ? "Next" : null;
     const nextButton = createButtonWithImage(
       nextText,
       nextUrl,
