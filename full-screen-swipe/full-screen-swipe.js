c16bb5a8543e2c59e14ef796e1d95c1d65e8b075
HarpoonProductions <110096521+HarpoonProductions@users.noreply.github.com>
2024-11-25 13:42:39 +0000

    Update full-screen-swipe.js

--- full-screen-swipe/full-screen-swipe.js
+++ full-screen-swipe/full-screen-swipe.js
@@ -42,17 +42,25 @@
       navigationContainer.classList.add("glide__arrows");
       navigationContainer.setAttribute("data-glide-el", "controls");
 
-      // Create prev button
+      // Create prev button with image
       const prevButton = document.createElement("button");
       prevButton.classList.add("glide__arrow", "glide__arrow--left");
       prevButton.setAttribute("data-glide-dir", "<");
-      prevButton.innerHTML = "‹";
+      const prevImg = document.createElement("img");
+      prevImg.src =
+        "https://harpn.s3.eu-west-2.amazonaws.com/harpoon/noun-chevron-1746062-FFFFFF.png";
+      prevImg.alt = "Previous";
+      prevButton.appendChild(prevImg);
 
-      // Create next button
+      // Create next button with image
       const nextButton = document.createElement("button");
       nextButton.classList.add("glide__arrow", "glide__arrow--right");
       nextButton.setAttribute("data-glide-dir", ">");
-      nextButton.innerHTML = "›";
+      const nextImg = document.createElement("img");
+      nextImg.src =
+        "https://harpn.s3.eu-west-2.amazonaws.com/harpoon/noun-chevron-right-1746094-FFFFFF.png";
+      nextImg.alt = "Next";
+      nextButton.appendChild(nextImg);
 
       // Add buttons to navigation container
       navigationContainer.appendChild(prevButton);
