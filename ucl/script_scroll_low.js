df5b703ff956c8afaa48b2411a25429ebba78c10
HarpoonProductions <110096521+HarpoonProductions@users.noreply.github.com>
2025-04-06 21:49:33 +0100

    Update script_scroll_low.js

--- ucl/script_scroll_low.js
+++ ucl/script_scroll_low.js
@@ -1,5 +1,6 @@
 /* Re-use of this code on stories not produced by Harpoon Productions is not permitted */
 (function () {
+  console.log('test THIS IS NEW')
   let currentPageIndex = null;
   var logoUrl = "https://harpn.s3.eu-west-2.amazonaws.com/ucl/ucl_icon.jpg";
   var logoUrlInner =
