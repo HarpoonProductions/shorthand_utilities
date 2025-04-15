9e8e8eab35b04c1f52d818903156aa98668d5052
HarpoonProductions <110096521+HarpoonProductions@users.noreply.github.com>
2025-04-15 19:59:01 +0100

    Update script_scroll_low_no_nav.js

--- ucl/script_scroll_low_no_nav.js
+++ ucl/script_scroll_low_no_nav.js
@@ -1,12 +1,12 @@
 /* Re-use of this code on stories not produced by Harpoon Productions is not permitted */
 (function () {
+  const links = [];
   let currentPageIndex = null;
   var logoUrl = "https://harpn.s3.eu-west-2.amazonaws.com/ucl/ucl_icon.jpg";
   var logoUrlInner =
     "https://harpn.s3.eu-west-2.amazonaws.com/ucl/ucl-logo-purple.png";
 
   function extractLinks() {
-    const links = [];
     const currentUrl = window.location.href;
 
     function getLink(node) {
