804a96fa543c60ecc9440098342a8682dad7f739
HarpoonProductions <110096521+HarpoonProductions@users.noreply.github.com>
2025-06-19 16:17:21 +0100

    Update script_scroll_low.js
    hand-fixed image urls

--- cavMag/cavMag33/script_scroll_low.js
+++ cavMag/cavMag33/script_scroll_low.js
@@ -2,9 +2,9 @@
 (function () {
   let currentPageIndex = null;
   var logoUrl =
-    "https://harpn.s3.eu-west-2.amazonaws.com/cavmag/cavmag_logo_2.jpg";
+    "https://harpn.s3.eu-west-2.amazonaws.com/cavmag/cavmag33/cavmag_logo_edition33.jpg";
   var logoUrlInner =
-    "https://harpn.s3.eu-west-2.amazonaws.com/cavmag/cavmag_wide.jpg";
+    "https://harpn.s3.eu-west-2.amazonaws.com/cavmag/cavmag33/cavmag_wide_edition33.jpg";
 
   function extractLinks() {
     const links = [];
