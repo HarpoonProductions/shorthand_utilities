c0260942adf3430ab1f0bac5fa59dd6baa305581
HarpoonProductions <110096521+HarpoonProductions@users.noreply.github.com>
2026-04-10 19:32:56 +0100

    Update searchUserNew-commemoration-fixed.js

--- imperial/searchUserNew-commemoration-fixed.js
+++ imperial/searchUserNew-commemoration-fixed.js
@@ -1130,7 +1130,7 @@ class TabOrderManager {
           )
           .forEach((el) => {
             if (el.classList.contains("popup-close"))
-              console.log("logging", el);
+              console.log("logging", el, el.getAttribute("tabindex"));
             if (el.getAttribute("tabindex") !== "-1") return;
             const text =
               el.textContent?.trim().slice(0, 30) || el.tagName.toLowerCase();
